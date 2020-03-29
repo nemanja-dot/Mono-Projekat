942e91daaeae0459e521d5b7944f2467b17669e9
Nemanja Mišljenović <nemanja.misljenovic93@gmail.com>
2020-03-29 18:02:45 +0200

    Added VehicleMakeServiceTest and VehicleModelServiceTest

--- Mono_Project/Project.Repository.Tests/VehicleMakeRepository/VehicleMakeRepositoryTest.cs
+++ Mono_Project/Project.Repository.Tests/VehicleMakeRepository/VehicleMakeRepositoryTest.cs
@@ -11,11 +11,13 @@ using Project.Repository.API;
 using Project.Repository.Common.Interfaces.API;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using FluentAssertions;
 
 namespace Project.Repository.Tests.VehicleMakeRepository
 {
     public class VehicleMakeRepositoryTest
     {
+        /*
         private VehicleMake GetTestMake()
         {
             var vehicleMake = new VehicleMake()
@@ -27,10 +29,11 @@ namespace Project.Repository.Tests.VehicleMakeRepository
             };
             return vehicleMake;
         }
+        */
 
         // FindAll
         [Fact]
-        public void GetAll_TestClassObjectPassed_ProperMethodCalled()
+        public void FindAll_FindAllVehicles_ReturnTestList()
         {
             // Arrange
             var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
@@ -51,218 +54,164 @@ namespace Project.Repository.Tests.VehicleMakeRepository
             var result = repository.FindAll();
 
             // Assert
-            Assert.Equal(testList, result.ToList());
+            // Assert.Equal(testList, result.ToList());
+            var okResult = result.Should().Equal(testList);
+
         }
 
         // FindByCondition
         [Fact]
-        public void FindAll_ReturnsFindVehicleMake_FindVehicleMake()
+        public void FindAll_FindVehicleMake_ReturnFindVehicleMake()
         {
             // Arrange
 
-            var testObject = new List<VehicleMake>
-            {
-                new VehicleMake()
-                {
-                    Id = 2,
-                    Name = "Test",
-                    Abrv = "T",
-                    VehicleModels = null,
-                    }
-            };
-
-            int testId = 2;
-            var testObject1 = new VehicleMake() { Id = 1 };
-            var testList = new List<VehicleMake>() {testObject1};
-
-            // Act
-            var dbContextMock = new Mock<ApplicationContext>();
+            var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
+            var testList = new List<VehicleMake>() { testObject };
+           
             var dbSetMock = new Mock<DbSet<VehicleMake>>();
-
             dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
             dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
             dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
             dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
 
+
+            var dbContextMock = new Mock<ApplicationContext>();
             dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
-            //dbSetMock.Setup(x => x.AsQueryable()).Returns(testObject.AsQueryable);
-            // dbSetMock.Setup(x => x.Where(testObject => testObject.Id == testId)).Returns(testObject.AsQueryable());
 
 
             // Act
-            var vehicleMakeRepository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
-            var vehicleMakes = vehicleMakeRepository.FindByCondition(x => x.Id == 1);
+            var repository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
+            var result = repository.FindByCondition(x => x.Id == testObject.Id);
 
 
             //Assert
-            Assert.Equal(testList, vehicleMakes.ToList());
-            // dbContextMock.Verify(x => x.Set<VehicleMake>());
-            // dbSetMock.Verify(x => x.AsQueryable());
+            //Assert.Equal(testList, vehicleMakes.ToList());
+            var okResult = result.Should().Equal(testList);
+        }
+        [Fact]
+        public void FindAll_FindVehicleMake_ReturnNoFoundVehicleMake()
+        {
+            // Arrange
+
+            var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
+            var testList = new List<VehicleMake>() { testObject };
+
+            var dbSetMock = new Mock<DbSet<VehicleMake>>();
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
+
+
+            // Act
+            var repository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
+            var result = repository.FindByCondition(x => x.Id == 5); // !=  testObject.Id
+
+
+            //Assert
+            //Assert.Equal(testList, vehicleMakes.ToList());
+            var okResult = result.Should().BeEmpty();
         }
 
         // Created
         [Fact]
-        public void Created_Returns_CreatedVehicleMake()
+        public void Created_CreatedVehicleMake_ReturnTrue()
         {
             // Arrange
 
-            var testObject = new List<VehicleMake>
-            {
-                new VehicleMake()
-                {
-                    Id = 2,
-                    Name = "Test",
-                    Abrv = "T",
-                    VehicleModels = null,
-                    }
-            };
-
-            int testId = 2;
-            var testObject1 = new VehicleMake();
+            var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
+            var testList = new List<VehicleMake>() { testObject };
 
             // Act
-            var dbContextMock = new Mock<ApplicationContext>();
+            
             var dbSetMock = new Mock<DbSet<VehicleMake>>();
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            dbSetMock.Setup(x => x.Add(testObject));
 
+            var dbContextMock = new Mock<ApplicationContext>();
             dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
-            // dbSetMock.Setup(x => x.AsQueryable()).Returns(testObject.AsQueryable);
-            // dbSetMock.Setup(x => x.Where(testObject => testObject.Id == testId)).Returns(testObject.AsQueryable());
-            dbSetMock.Setup(x => x.Add(testObject1));
 
             // Act
-            var vehicleMakeRepository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
-            var vehicleMakes = vehicleMakeRepository.Create(testObject1);
+            var repository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
+            var result = repository.Create(testObject);
 
 
             //Assert
-            dbContextMock.Verify(x => x.Set<VehicleMake>());
-            dbSetMock.Verify(x => x.Add(It.Is<VehicleMake>(y => y == testObject1)));
+            // Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
         }
 
         //Update
         [Fact]
-        public void Update_Returns_UpdateVehicleMake()
+        public void Update_UpdateVehicleMake_ReturnTrue()
         {
             // Arrange
 
-            var testObject = new List<VehicleMake>
-            {
-                new VehicleMake()
-                {
-                    Id = 2,
-                    Name = "Test",
-                    Abrv = "T",
-                    VehicleModels = null,
-                    }
-            };
-
-            int testId = 2;
-            var testObject1 = new VehicleMake();
-
-            // Act
-            var dbContextMock = new Mock<ApplicationContext>();
+            var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
+            var testList = new List<VehicleMake>() { testObject };
+            
+            
             var dbSetMock = new Mock<DbSet<VehicleMake>>();
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            dbSetMock.Setup(x => x.Update(testObject));
 
+            var dbContextMock = new Mock<ApplicationContext>();
             dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
-            // dbSetMock.Setup(x => x.AsQueryable()).Returns(testObject.AsQueryable);
-            // dbSetMock.Setup(x => x.Where(testObject => testObject.Id == testId)).Returns(testObject.AsQueryable());
-            dbSetMock.Setup(x => x.Update(testObject1));
+            
 
             // Act
-            var vehicleMakeRepository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
-            var vehicleMakes = vehicleMakeRepository.Update(testObject1);
+            var repository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
+            var result = repository.Update(testObject);
 
 
             //Assert
-            dbContextMock.Verify(x => x.Set<VehicleMake>());
-            dbSetMock.Verify(x => x.Update(It.Is<VehicleMake>(y => y == testObject1)));
+            //Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
         }
 
 
 
         // Delete
         [Fact]
-        public void Deleted_Returns_DeletedVehicleMake()
+        public void Deleted_DeletedVehicleMake_ReturnTrue()
         {
             // Arrange
 
-            var testObject = new List<VehicleMake>
-            {
-                new VehicleMake()
-                {
-                    Id = 2,
-                    Name = "Test",
-                    Abrv = "T",
-                    VehicleModels = null,
-                    }
-            };
-
-            int testId = 2;
-            var testObject1 = new VehicleMake();
+            var testObject = new VehicleMake() { Id = 1, Name = "Test", Abrv = "T", VehicleModels = null };
+            var testList = new List<VehicleMake>() { testObject };
 
             // Act
-            var dbContextMock = new Mock<ApplicationContext>();
+            
             var dbSetMock = new Mock<DbSet<VehicleMake>>();
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleMake>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
 
+
+            var dbContextMock = new Mock<ApplicationContext>();
             dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
-            // dbSetMock.Setup(x => x.AsQueryable()).Returns(testObject.AsQueryable);
-            // dbSetMock.Setup(x => x.Where(testObject => testObject.Id == testId)).Returns(testObject.AsQueryable());
-            dbSetMock.Setup(x => x.Remove(testObject1));
 
             // Act
-            var vehicleMakeRepository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
-            var vehicleMakes = vehicleMakeRepository.Delete(testObject1);
+            var repository = new RepositoryBase<VehicleMake>(dbContextMock.Object);
+            var result = repository.Delete(testObject);
 
 
             //Assert
-            dbContextMock.Verify(x => x.Set<VehicleMake>());
-            dbSetMock.Verify(x => x.Remove(It.Is<VehicleMake>(y => y == testObject1)));
+            //Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
         }
 
-        /*
-        [Fact]
-        public void FindAll_Returns_Product()
-        {
-            //Setup DbContext and DbSet mock
-            var dbContextMock = new Mock<ApplicationContext>();
-            var dbSetMock = new Mock<DbSet<VehicleMake>>();
-            dbSetMock.Setup(s => s.AsQueryable());
-            dbContextMock.Setup(s => s.Set<VehicleMake>()).Returns(dbSetMock.Object);
-
-            //Execute method of SUT 
-            var vehicleMakeRepository = new API.VehicleMakeRepository(dbContextMock.Object);
-            var product = vehicleMakeRepository.FindAll();
-
-            //Assert
-            Assert.NotNull(product);
-            Assert.IsAssignableFrom<VehicleMake>(product);
-        } 
         
-
-        [Fact]
-        public void Add_WhenHaveNoEmail()
-        {
-            IVehicleMakeRepository sut = GetInMemoryPersonRepository();
-            VehicleMake vehicleMake = new VehicleMake()
-            {
-               Id = 2,
-               Name = "TestRepo",
-               Abrv = "TRepo",
-               VehicleModels = null
-            };
-
-            VehicleMake savedPerson = sut.Create.(entity);
-
-
-            Assert.Equal(2, savedPerson.Id);
-            Assert.Equal("TestRepo", savedPerson.Name);
-            Assert.Equal("TRepo", savedPerson.Abrv);
-            Assert.Null(savedPerson.VehicleModels);
-        }
-
-        private IVehicleMakeRepository GetInMemoryPersonRepository()
-        {
-            throw new NotImplementedException();
-        }*/
     }
 }
---
+++ Mono_Project/Project.Repository.Tests/VehicleModelRepository/VehicleModelRepositoryTest.cs
@@ -0,0 +1,195 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Project.DAL.Context;
+using Project.Model.Model;
+using Project.Repository.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Project.Repository.Tests.VehicleModelRepository
+{
+    public class VehicleModelRepositoryTest
+    {
+        // FindAll
+        [Fact]
+        public void FindAll_FindAllVehicles_ReturnTestList()
+        {
+            // Arrange
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            dbSetMock.Setup(x => x.AsQueryable()).Returns(testList.AsQueryable);
+
+            var context = new Mock<ApplicationContext>();
+            context.Setup(x => x.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(context.Object);
+            var result = repository.FindAll();
+
+            // Assert
+            // Assert.Equal(testList, result.ToList());
+            var okResult = result.Should().Equal(testList);
+        }
+
+        // FindByCondition
+        [Fact]
+        public void FindAll_FindVehicleModel_ReturnFindVehicleModel()
+        {
+            // Arrange
+
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(dbContextMock.Object);
+            var result = repository.FindByCondition(x => x.Id == testObject.Id);
+
+
+            //Assert
+            //Assert.Equal(testList, vehicleMakes.ToList());
+            var okResult = result.Should().Equal(testList);
+        }
+        [Fact]
+        public void FindAll_FindVehicleModel_ReturnNoFoundVehicleModel()
+        {
+            // Arrange
+
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(dbContextMock.Object);
+            var result = repository.FindByCondition(x => x.Id == 5); // !=  testObject.Id
+
+
+            //Assert
+            //Assert.Equal(testList, vehicleMakes.ToList());
+            var okResult = result.Should().BeEmpty();
+        }
+
+        // Created
+        [Fact]
+        public void Created_CreatedVehicleModel_ReturnTrue()
+        {
+            // Arrange
+
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+            // Act
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            dbSetMock.Setup(x => x.Add(testObject));
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(dbContextMock.Object);
+            var result = repository.Create(testObject);
+
+
+            //Assert
+            // Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
+        }
+
+        //Update
+        [Fact]
+        public void Update_UpdateVehicleModel_ReturnTrue()
+        {
+            // Arrange
+
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+            dbSetMock.Setup(x => x.Update(testObject));
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(dbContextMock.Object);
+            var result = repository.Update(testObject);
+
+
+            //Assert
+            //Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
+        }
+
+        // Delete
+        [Fact]
+        public void Deleted_DeletedVehicleModel_ReturnTrue()
+        {
+            // Arrange
+
+            var testObject = new VehicleModel() { Id = 1, Name = "Test", Abrv = "T" };
+            var testList = new List<VehicleModel>() { testObject };
+
+            // Act
+
+            var dbSetMock = new Mock<DbSet<VehicleModel>>();
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Provider).Returns(testList.AsQueryable().Provider);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.Expression).Returns(testList.AsQueryable().Expression);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.ElementType).Returns(testList.AsQueryable().ElementType);
+            dbSetMock.As<IQueryable<VehicleModel>>().Setup(x => x.GetEnumerator()).Returns(testList.AsQueryable().GetEnumerator());
+
+
+            var dbContextMock = new Mock<ApplicationContext>();
+            dbContextMock.Setup(s => s.Set<VehicleModel>()).Returns(dbSetMock.Object);
+
+            // Act
+            var repository = new RepositoryBase<VehicleModel>(dbContextMock.Object);
+            var result = repository.Delete(testObject);
+
+
+            //Assert
+            //Assert.True(result.IsCompletedSuccessfully);
+            var okResult = result.Should().NotBeNull();
+        }
+    }
+}
