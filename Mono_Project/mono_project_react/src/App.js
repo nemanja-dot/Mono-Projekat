15a8597fa887ca9c802a63291ef5e1bf441d2a3a
Nemanja Mišljenović <nemanja.misljenovic93@gmail.com>
2020-04-14 11:49:47 +0200

    Added Layout

--- Mono_Project/mono_project_react/src/App.js
+++ Mono_Project/mono_project_react/src/App.js
@@ -6,6 +6,7 @@ import {
   NavLink,
 } from "react-router-dom";
 import "./App.css";
+import { Layout } from "./Layouts/Layout";
 import vehicleMakeList from "./Components/vehicleMakeList";
 import vehicleModelList from "./Components/vehicleModelList";
 import vehicleMakeCreate from "./Components/vehicleMakeCreate";
@@ -13,70 +14,103 @@ import vehicleModelCreate from "./Components/vehicleModelCreate";
 import vehicleMakeEdit from "./Components/vehicleMakeEdit";
 import vehicleModelEdit from "./Components/vehicleModelEdit";
 
-class App extends Component {
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <nav className="navbar navbar-expand navbar-dark bg-dark">
+//             <a href="/" className="navbar-brand">
+//               Mono React
+//             </a>
+//             <NavLink
+//               variant="info"
+//               className="item"
+//               activeClassName="active"
+//               exact
+//               to="/Components/vehicleMakeList"
+//             >
+//               Vehicle Make List
+//             </NavLink>
+//             <NavLink
+//               variant="info"
+//               className="item"
+//               activeClassName="active"
+//               exact
+//               to="/Components/vehicleModelList"
+//             >
+//               Vehicle Model List
+//             </NavLink>
+//             <div className="navbar-nav mr-auto"></div>
+//           </nav>
+//           <div className="container mt-3">
+//             <Switch>
+//               <Route
+//                 exact
+//                 path="/Components/vehicleMakeList"
+//                 component={vehicleMakeList}
+//               />
+//               <Route
+//                 exact
+//                 path="/Components/vehicleModelList"
+//                 component={vehicleModelList}
+//               />
+//               <Route
+//                 path="/Components/vehicleMakeCreate"
+//                 component={vehicleMakeCreate}
+//               />
+//               <Route
+//                 path="/Components/vehicleModelCreate"
+//                 component={vehicleModelCreate}
+//               />
+//               <Route
+//                 path="/Components/vehicleMakeEdit"
+//                 component={vehicleMakeEdit}
+//               />
+//               <Route
+//                 path="/Components/vehicleModelEdit"
+//                 component={vehicleModelEdit}
+//               />
+//               {/* <Route path="/Common/vehicleMakeDelete" component={vehicleMakeDelete} /> */}
+//             </Switch>
+//           </div>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+export default class App extends Component {
+  static displayName = App.name;
+
   render() {
     return (
-      <Router>
-        <div>
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="/" className="navbar-brand">
-              Mono React
-            </a>
-            <NavLink
-              variant="info"
-              className="item"
-              activeClassName="active"
-              exact
-              to="/Components/vehicleMakeList"
-            >
-              Vehicle Make List
-            </NavLink>
-            <NavLink
-              variant="info"
-              className="item"
-              activeClassName="active"
-              exact
-              to="/Components/vehicleModelList"
-            >
-              Vehicle Model List
-            </NavLink>
-            <div className="navbar-nav mr-auto"></div>
-          </nav>
-          <div className="container mt-3">
-            <Switch>
-              <Route
-                exact
-                path="/Components/vehicleMakeList"
-                component={vehicleMakeList}
-              />
-              <Route
-                exact
-                path="/Components/vehicleModelList"
-                component={vehicleModelList}
-              />
-              <Route
-                path="/Components/vehicleMakeCreate"
-                component={vehicleMakeCreate}
-              />
-              <Route
-                path="/Components/vehicleModelCreate"
-                component={vehicleModelCreate}
-              />
-              <Route
-                path="/Components/vehicleMakeEdit"
-                component={vehicleMakeEdit}
-              />
-              <Route
-                path="/Components/vehicleModelEdit"
-                component={vehicleModelEdit}
-              />
-              {/* <Route path="/Common/vehicleMakeDelete" component={vehicleMakeDelete} /> */}
-            </Switch>
-          </div>
-        </div>
-      </Router>
+      <Layout>
+        <Route
+          exact
+          path="/Components/vehicleMakeList"
+          component={vehicleMakeList}
+        />
+        <Route
+          path="/Components/vehicleModelList"
+          component={vehicleModelList}
+        />
+        <Route
+          path="/Components/vehicleMakeCreate"
+          component={vehicleMakeCreate}
+        />
+        <Route
+          path="/Components/vehicleModelCreate"
+          component={vehicleModelCreate}
+        />
+        <Route path="/Components/vehicleMakeEdit" component={vehicleMakeEdit} />
+        <Route
+          path="/Components/vehicleModelEdit"
+          component={vehicleModelEdit}
+        />
+      </Layout>
     );
   }
 }
-
-export default App;
